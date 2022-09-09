d780334b09228f9ec1dac18ed8314dcdfff18712
pauel3312 <pau.lacazedieu@gmail.com>
2022-09-09 08:57:36 +0200

    Test implementation

---
+++ tests.py
@@ -0,0 +1,12 @@
+import pytest
+import Morpion
+
+
+def test_morpion_move():
+    game = Morpion.Player()
+    game.do_move(3)
+    assert game.layout_str == "000001000"
+    game.do_move(3)
+    assert game.layout_str == "000001000"
+    game.do_move(4)
+    assert game.layout_str == "000021000"
